6ec0432388c640aec8f61896edaaf7cc772e8ec0
olenevoid <mazekunov@gmail.com>
2026-04-02 17:12:18 +0500

    Create index.html that will redirect to index1.html

--- render_webpage.py
+++ render_webpage.py
@@ -1,22 +1,22 @@
-from livereload import Server
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-from os import path, makedirs
 import json
+from os import makedirs, path
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 from more_itertools import chunked
 
-
-TEMPLATES_FOLDER = 'templates'
-HTML_FOLDER = '.'
-STATIC = 'static'
-MEDIA = 'media'
+TEMPLATES_FOLDER = "templates"
+HTML_FOLDER = "."
+STATIC = "static"
+MEDIA = "media"
 NUMBER_OF_COLUMNS = 2
 ITEMS_ON_PAGE = 10
-PAGES_FOLDER = 'pages'
+PAGES_FOLDER = "pages"
 
 
 def load_books():
-    filepath = f'{MEDIA}/meta_data.json'
-    with open(filepath, 'r', encoding='utf-8') as file:
+    filepath = f"{MEDIA}/meta_data.json"
+    with open(filepath, "r", encoding="utf-8") as file:
         books = json.loads(file.read())
         pages_of_books = chunked(books, ITEMS_ON_PAGE)
 
@@ -31,12 +31,12 @@ def get_page_navigation(books, current_page):
     has_next = next_ <= total_pages
 
     page_navigation = {
-        'total_pages': total_pages,
-        'current_page': current_page,
-        'has_previous': has_previous,
-        'has_next': has_next,
-        'next_page': next_ if next_ else None,
-        'previous_page': previous if previous else None
+        "total_pages": total_pages,
+        "current_page": current_page,
+        "has_previous": has_previous,
+        "has_next": has_next,
+        "next_page": next_ if next_ else None,
+        "previous_page": previous if previous else None,
     }
 
     return page_navigation
@@ -44,14 +44,14 @@ def get_page_navigation(books, current_page):
 
 def rebuild():
     jinja_environment = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"]),
     )
 
     books = load_books()
 
     template = jinja_environment.get_template(
-        path.join(TEMPLATES_FOLDER, 'index.html')
+        path.join(TEMPLATES_FOLDER, "index.html")
     )
 
     makedirs(PAGES_FOLDER, exist_ok=True)
@@ -61,11 +61,18 @@ def rebuild():
             books=page,
             media=MEDIA,
             static=STATIC,
-            page_navigation=get_page_navigation(books, number)
+            page_navigation=get_page_navigation(books, number),
         )
 
-        with open(f'{PAGES_FOLDER}/index{number}.html', 'w', encoding="utf8") as file:
+        with open(
+            f"{PAGES_FOLDER}/index{number}.html", "w", encoding="utf8"
+        ) as file:
             file.write(rendered_page)
+
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(
+            '<meta http-equiv="refresh" ' 'content="0; url=pages/index1.html">'
+        )
 
 
 def main():
@@ -75,12 +82,12 @@ def main():
 
     server = Server()
 
-    server.watch(f'{TEMPLATES_FOLDER}/*.html', rebuild)
+    server.watch(f"{TEMPLATES_FOLDER}/*.html", rebuild)
 
     server.watch(STATIC)
 
     server.serve(root=HTML_FOLDER)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
